e775862ded10cf50e7de7da25891e18c8fed41c0
Rannya7x <rannya7x@gmail.com>
2025-08-30 21:01:26 -0400

    feat: add music functions

--- src/musica.c
+++ src/musica.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "musica.h"
+#include <assert.h>
+#include "../include/musica.h"
 
 struct musica {
     char nome[60];
@@ -12,8 +13,9 @@ struct musica {
     int ano;
 };
 
-t_musica* musica_criar(void* nome, void* genero, int duracao, void* album, void* cantor, int ano) {
+t_musica* musica_criar(char* nome, char* genero, int duracao, char* album, char* cantor, int ano) {
     t_musica* msc = malloc(sizeof(t_musica));
+    assert(msc != NULL);
     strcpy(msc->nome, nome);
     strcpy(msc->genero, genero);
     msc->duracao = duracao;
@@ -24,14 +26,22 @@ t_musica* musica_criar(void* nome, void* genero, int duracao, void* album, void*
 }
 
 void musica_imprimir(void* p) {
-    t_musica msc = *(t_musica*)p;
-    printf("Nome: %s\n", msc.nome);
+    t_musica* msc = (t_musica*)p;
+    assert(msc != NULL);
+    printf( "Nome: %s\n", msc->nome);
 }
 
 int musica_cmp_nome(void* carga, void* chave) {
-    // Implemente o seu código aqui
+    t_musica* msc = (t_musica*)carga;
+    char* nome_chave = (char*)chave;
+    assert(msc != NULL);
+    assert(chave != NULL);
+    return strcmp(msc->nome, nome_chave);
 }
 
 void musica_destruir(void* p) {
-    // Implemente o seu código aqui
+    t_musica* msc = (t_musica*)p;
+    assert(msc != NULL);
+    free(msc);
+    return NULL;
 }
